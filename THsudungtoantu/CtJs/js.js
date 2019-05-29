f1246696d2c048c930c4bf1f960946ebb8d3ee70
Nguyentu1996 <50936704+Nguyentu1996@users.noreply.github.com>
2019-05-29 20:45:13 +0700

    ok

--- THsudungtoantu/CtJs/js.js
+++ THsudungtoantu/CtJs/js.js
@@ -1,8 +1,10 @@
 let inputWidth;
-let inputHeight;
-inputWidth = prompt("Enter the width");
-inputHeight = prompt("Enter the height");
+   let inputHeight;
+   inputWidth = prompt("Enter the width")
+   inputHeight = prompt("Enter the height")
 let width = parseInt(inputWidth);
+document.write("Enter width :" + width);
 let height = parseInt(inputHeight);
+document.write("Enter height:" + height);
 let area = width * height;
 document.write("The area is: " + area);
